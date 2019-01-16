b975297e4785f230c874eaf468b3491c6a814c33
Leandro Santos <leandro.santos@atsnet.com.br>
2019-01-16 15:32:01 -0200

    07. Bridge

---
+++ 07. Bridge/Exercise.cs
@@ -0,0 +1,42 @@
+﻿namespace Bridge.Exercise
+{
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+        public Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {renderer.WhatToRenderAs}";
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer renderer) : base(renderer) { Name = "Triangle"; }
+    }
+
+    public class Square : Shape
+    {
+        public Square(IRenderer renderer) : base(renderer) { Name = "Square"; }
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs => "lines";
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs => "pixel";
+    }
+}
---
+++ 07. Bridge/Program.cs
@@ -0,0 +1,79 @@
+﻿using Autofac;
+using System;
+using static System.Console;
+
+namespace Bridge
+{
+    interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing a circle of radius {radius}");
+        }
+    }
+
+    class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing pixels for circle of radius {radius}");
+        }
+    }
+
+    abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer ?? throw new ArgumentNullException(paramName:nameof(renderer));
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    class Circle : Shape
+    {
+        private float radius;
+
+        public Circle(IRenderer renderer, float radius) : base(renderer)
+        {
+            this.renderer = renderer ?? throw new ArgumentNullException(paramName:nameof(renderer));
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<VectorRenderer>().As<IRenderer>().SingleInstance();
+            cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(), p.Positional<float>(0)));
+            using (var c = cb.Build())
+            {
+                var circle = c.Resolve<Circle>(new PositionalParameter(0, 5.0f));
+                circle.Draw();
+                circle.Resize(2.0f);
+                circle.Draw();
+            }
+
+        }
+    }
+}
