c96a07c85f78df6f41484f0f7c7b08abdaeaf9a3
Leandro Santos <leandro.santos@atsnet.com.br>
2019-01-16 10:56:47 -0200

    06. Adapter

---
+++ 06. Adapter/Point.cs
@@ -0,0 +1,34 @@
+﻿namespace Adapter
+{
+    class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        protected bool Equals(Point point)
+        {
+            return X == point.X && Y == point.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+    }
+}
