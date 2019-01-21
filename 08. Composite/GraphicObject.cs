b60fcd894b32953fa71e5d09be8da52060da8017
Leandro Santos <leandro.santos@atsnet.com.br>
2019-01-21 14:25:41 -0200

    08. Composite

---
+++ 08. Composite/GraphicObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+
+    class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color { get; set; }
+
+        private Lazy<List<GraphicObject>> _children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => _children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+              .Append(!string.IsNullOrWhiteSpace(Color) ? $"{Color} " : string.Empty)
+              .AppendLine(Name);
+
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 1);
+            return sb.ToString();
+        }
+    }
+
+    class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+}
---
+++ 08. Composite/NeuralNetworks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    static class NeuralNetworksExtensionMethods
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other)) return;
+
+            foreach (var from in self)
+            {
+                foreach (var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+            }
+        }
+    }
+
+    class Neuron : IEnumerable<Neuron>
+    {
+        public float Value { get; set; }
+
+        public List<Neuron> In { get; } = new List<Neuron>();
+        public List<Neuron> Out { get; } = new List<Neuron>();
+
+        public void ConnectTo(Neuron other)
+        {
+            Out.Add(other);
+            other.In.Add(this);
+        }
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+}
